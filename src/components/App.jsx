6f7bc52f95168a17548ba97994e88337958b9ffe
heorh11 <gheorghe.filip225@gmail.com>
2023-11-26 17:17:27 +0200

    all done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import FriendList from './FriendList/FriendList';
+import FriendList from './FriendsList/FriendList';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from '../user.json';
 import data from '../data.json';
